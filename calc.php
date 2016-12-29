ecfb6c56b89c240ea29186fe100c8146e1f163b3
watermelonade <jake.titherley@gmail.com>
2016-12-29 00:44:09 -0800

    small changes to style, some bug fixes

--- calc.php
+++ calc.php
@@ -18,17 +18,17 @@
 		<td><?php 
 			for($i = 0; $i < 3; $i++) { ?>
 				<input type="radio" name="tip" <?php if($i == 0){ echo "checked";}?> value="<?php echo .10 + $i*.05; ?>" 
-					<?php if(isset($_POST['tip' . $i]) && $_POST['tip' . $i] == 'No')  echo ' checked="checked"';?>/> <?php echo 10 + $i*5; ?>%
+					<?php if(isset($_POST['tip']) && $_POST['tip'] == 'No')  echo ' checked="checked"';?>/> <?php echo 10 + $i*5; ?>%
 			<?php } ?>
 		<tr>
-		<td>Custom</td>
-			<td><input type="radio" name="tip"><input type="text" name="other_tip" /></td>
+		<td></td>
+			<td><input type="radio" name="tip">Custom<input type="text" name="tip" />%</td>
 		</tr>
 		<tr>
 			<td># of People:</td>
 			<td><input type="text" name="num_people" value="1" /> </td>
 		</tr><tr>
-			<td><center><input type="submit" name="calculate" value="Calculate" /></center></td>
+			<center><td><input type="submit" name="calculate" value="Calculate" /></td></center>
 		</tr>
 		</table>
 	</form>
@@ -42,13 +42,28 @@
 			$tip = $_POST['tip']; 
 			$bill = $_POST["bill"]; 
 			
+			if($tip > 1)
+				$tip = 1/$tip;
+			
 			$stip = preg_replace("[^-?\\d+(\\.\\d+)?]","",$tip);
 			$sbill = preg_replace("[^-?\\d+(\\.\\d+)?]","",$bill);
 			if(!$stip && !$sbill && !empty($bill) )
 			{
 			?>
-			<td> <?php echo "Tip: $" . $tip*$bill . "\n"; ?></td><br>
-			<td> <?php echo "Total: $" . ($tip*$bill + $bill);?></td>
+			<td> <?php echo "Tip: $" . round($tip*$bill,2) . "\n"; ?></td><br>
+			<td> <?php echo "Total: $" . round($tip*$bill + $bill,2);?></td>
+			
+			<?php 
+			if(isset($_POST['num_people'])) 
+			{
+				$split = $_POST['num_people'];
+				if($split > 1)
+				{
+					echo "\nTip per person   : " . round(($tip*$bill)/$split,2);
+					echo "\nTotal per person : " . round(round(($tip*$bill + $bill)/$split,2));
+				}
+			}?>
+			
 			<?php 
 			} else {
 				if($stip)
